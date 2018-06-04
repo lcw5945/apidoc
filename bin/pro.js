c6526714c4241cd66cd496419725660ee10ac99b
李春伟 <lichunwei@hefantv.com>
2018-06-04 15:48:51 +0800

    modify bin

--- bin/pro.js
+++ bin/pro.js
@@ -1,119 +1,121 @@
-'use strict';
+"use strict";
 
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
+// process.traceDeprecation = true;
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on('unhandledRejection', err => {
-    throw err;
+process.on("unhandledRejection", err => {
+  throw err;
 });
 
 // Ensure environment variables are read.
-require('../config/env');
+require("../config/env");
 
-const path = require('path');
-const chalk = require('chalk');
-const fs = require('fs-extra');
-const webpack = require('webpack');
-const config = require('../config/webpack.config.prod');
-const paths = require('../config/paths');
-const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
-const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
-const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
-const FileSizeReporter = require('react-dev-utils/FileSizeReporter');
+const path = require("path");
+const chalk = require("chalk");
+const fs = require("fs-extra");
+const webpack = require("webpack");
+const config = require("../config/webpack.config.prod");
+const paths = require("../config/paths");
+const checkRequiredFiles = require("react-dev-utils/checkRequiredFiles");
+const formatWebpackMessages = require("react-dev-utils/formatWebpackMessages");
+const printHostingInstructions = require("react-dev-utils/printHostingInstructions");
+const FileSizeReporter = require("react-dev-utils/FileSizeReporter");
 
-const measureFileSizesBeforeBuild = FileSizeReporter.measureFileSizesBeforeBuild;
+const measureFileSizesBeforeBuild =
+  FileSizeReporter.measureFileSizesBeforeBuild;
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
 const useYarn = fs.existsSync(paths.yarnLockFile);
 
 if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
-    process.exit(1);
+  process.exit(1);
 }
 
 measureFileSizesBeforeBuild(paths.appBuild)
-    .then(previousFileSizes => {
-        fs.emptyDirSync(paths.appBuild);
-        copyPublicFolder();
-        return build(previousFileSizes);
-    })
-    .then(
-        ({ stats, previousFileSizes, warnings }) => {
-            if (warnings.length) {
-                console.log(chalk.yellow('Compiled with warnings.\n'));
-                console.log(warnings.join('\n\n'));
-                console.log(
-                    '\nSearch for the ' +
-                    chalk.underline(chalk.yellow('keywords')) +
-                    ' to learn more about each warning.'
-                );
-                console.log(
-                    'To ignore, add ' +
-                    chalk.cyan('// eslint-disable-next-line') +
-                    ' to the line before.\n'
-                );
-            } else {
-                console.log(chalk.green('Compiled successfully.\n'));
-            }
+  .then(previousFileSizes => {
+    fs.emptyDirSync(paths.appBuild);
+    copyPublicFolder();
+    return build(previousFileSizes);
+  })
+  .then(
+    ({ stats, previousFileSizes, warnings }) => {
+      if (warnings.length) {
+        console.log(chalk.yellow("Compiled with warnings.\n"));
+        console.log(warnings.join("\n\n"));
+        console.log(
+          "\nSearch for the " +
+            chalk.underline(chalk.yellow("keywords")) +
+            " to learn more about each warning."
+        );
+        console.log(
+          "To ignore, add " +
+            chalk.cyan("// eslint-disable-next-line") +
+            " to the line before.\n"
+        );
+      } else {
+        console.log(chalk.green("Compiled successfully.\n"));
+      }
 
-            console.log('File sizes after gzip:\n');
-            printFileSizesAfterBuild(stats, previousFileSizes, paths.appBuild);
-            console.log();
+      console.log("File sizes after gzip:\n");
+      printFileSizesAfterBuild(stats, previousFileSizes, paths.appBuild);
+      console.log();
 
-            const appPackage = require(paths.appPackageJson);
-            const publicUrl = paths.publicUrl;
-            const publicPath = config.output.publicPath;
-            const buildFolder = path.relative(process.cwd(), paths.appBuild);
-            printHostingInstructions(
-                appPackage,
-                publicUrl,
-                publicPath,
-                buildFolder,
-                useYarn
-            );
-        },
-        err => {
-            console.log(chalk.red('Failed to compile.\n'));
-            console.log((err.message || err) + '\n');
-            process.exit(1);
-        }
-    );
+      const appPackage = require(paths.appPackageJson);
+      const publicUrl = paths.publicUrl;
+      const publicPath = config.output.publicPath;
+      const buildFolder = path.relative(process.cwd(), paths.appBuild);
+      printHostingInstructions(
+        appPackage,
+        publicUrl,
+        publicPath,
+        buildFolder,
+        useYarn
+      );
+    },
+    err => {
+      console.log(chalk.red("Failed to compile.\n"));
+      console.log((err.message || err) + "\n");
+      process.exit(1);
+    }
+  );
 
 function build(previousFileSizes) {
-    console.log('Creating an optimized ' + process.env.NODE_ENV + ' build...');
+  console.log("Creating an optimized " + process.env.NODE_ENV + " build...");
 
-    let compiler = webpack(config);
-    return new Promise((resolve, reject) => {
-        compiler.run((err, stats) => {
-            if (err) {
-                return reject(err);
-            }
-            const messages = formatWebpackMessages(stats.toJson({}, true));
-            if (messages.errors.length) {
-                return reject(new Error(messages.errors.join('\n\n')));
-            }
-            if (process.env.CI && messages.warnings.length) {
-                console.log(
-                    chalk.yellow(
-                        '\nTreating warnings as errors because process.env.CI = true.\n' +
-                        'Most CI servers set it automatically.\n'
-                    )
-                );
-                return reject(new Error(messages.warnings.join('\n\n')));
-            }
-            return resolve({
-                stats,
-                previousFileSizes,
-                warnings: messages.warnings,
-            });
-        });
+  let compiler = webpack(config);
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+      const messages = formatWebpackMessages(stats.toJson({}, true));
+      if (messages.errors.length) {
+        return reject(new Error(messages.errors.join("\n\n")));
+      }
+      if (process.env.CI && messages.warnings.length) {
+        console.log(
+          chalk.yellow(
+            "\nTreating warnings as errors because process.env.CI = true.\n" +
+              "Most CI servers set it automatically.\n"
+          )
+        );
+        return reject(new Error(messages.warnings.join("\n\n")));
+      }
+      return resolve({
+        stats,
+        previousFileSizes,
+        warnings: messages.warnings
+      });
     });
+  });
 }
 
 function copyPublicFolder() {
-    fs.copySync(paths.appPublic, paths.appBuild, {
-        dereference: true,
-        filter: file => file !== paths.appHtml,
-    });
+  fs.copySync(paths.appPublic, paths.appBuild, {
+    dereference: true,
+    filter: file => file !== paths.appHtml
+  });
 }
--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -2,155 +2,153 @@
  * Created by Cray on 2017/7/20.
  */
 
-const autoprefixer = require('autoprefixer');
-const path = require('path');
-const webpack = require('webpack');
-const eslintFormatter = require('react-dev-utils/eslintFormatter');
-const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
-const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
-const getClientEnvironment = require('./env');
-const paths = require('./paths');
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+const webpack = require("webpack");
+const eslintFormatter = require("react-dev-utils/eslintFormatter");
+const InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
+const getClientEnvironment = require("./env");
+const paths = require("./paths");
 
-const publicPath = '/';
-const publicUrl = '';
+const publicPath = "/";
+const publicUrl = "";
 const env = getClientEnvironment(publicUrl);
 
 module.exports = {
-    devtool: 'eval',
-    output: {
-        path: paths.appBuild,
-        filename: 'static/js/bundle.js',
-        chunkFilename: 'static/js/[name].chunk.js',
-        publicPath: publicPath,
-        devtoolModuleFilenameTemplate: info =>
-            path.resolve(info.absoluteResourcePath),
+  devtool: "eval",
+  output: {
+    path: paths.appBuild,
+    filename: "static/js/bundle.js",
+    chunkFilename: "static/js/[name].chunk.js",
+    publicPath: publicPath,
+    devtoolModuleFilenameTemplate: info =>
+      path.resolve(info.absoluteResourcePath)
+  },
+  resolve: {
+    modules: ["node_modules", paths.appNodeModules].concat(
+      process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+    ),
+    extensions: [".js", ".json", ".jsx"],
+    alias: {
+      "babel-runtime": path.dirname(
+        require.resolve("babel-runtime/package.json")
+      ),
+      "~actions": path.resolve(__dirname, "../src/actions"),
+      "~components": path.resolve(__dirname, "../src/components"),
+      "~containers": path.resolve(__dirname, "../src/containers"),
+      "~constants": path.resolve(__dirname, "../src/constants"),
+      "~pages": path.resolve(__dirname, "../src/pages"),
+      "~reducers": path.resolve(__dirname, "../src/reducers"),
+      "~utils": path.resolve(__dirname, "../src/utils"),
+      "~plugins": path.resolve(__dirname, "../src/plugins"),
+      "~api": path.resolve(__dirname, "../src/api"),
+      "~lib": path.resolve(__dirname, "../src/lib"),
+      "~common": path.resolve(__dirname, "../src/common")
     },
-    resolve: {
-        modules: ['node_modules', paths.appNodeModules].concat(
-            process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
-        ),
-        extensions: ['.js', '.json', '.jsx'],
-        alias: {
-            'babel-runtime': path.dirname(
-                require.resolve('babel-runtime/package.json')
-            ),
-            "~actions": path.resolve(__dirname, "../src/actions"),
-            "~components": path.resolve(__dirname, "../src/components"),
-            "~containers": path.resolve(__dirname, "../src/containers"),
-            "~constants": path.resolve(__dirname, "../src/constants"),
-            "~pages": path.resolve(__dirname, "../src/pages"),
-            "~reducers": path.resolve(__dirname, "../src/reducers"),
-            "~utils": path.resolve(__dirname, "../src/utils"),
-            "~plugins": path.resolve(__dirname, "../src/plugins"),
-            "~api": path.resolve(__dirname, "../src/api"),
-            "~lib": path.resolve(__dirname, "../src/lib"),
-            "~common": path.resolve(__dirname, "../src/common"),
-        },
-        plugins: [
-            new ModuleScopePlugin(paths.appSrc),
+    plugins: [new ModuleScopePlugin(paths.appSrc)]
+  },
+  module: {
+    strictExportPresence: true,
+    rules: [
+      // {
+      //     test: /\.(js|jsx)$/,
+      //     enforce: 'pre',
+      //     use: [
+      //         {
+      //             options: {
+      //                 formatter: eslintFormatter,
+      //                 baseConfig: {
+      //                     extends: [require.resolve(path.resolve(__dirname, "../config/eslint-config-react"))],
+      //                 },
+      //                 ignore: false,
+      //                 useEslintrc: false
+      //             },
+      //             loader: require.resolve('eslint-loader'),
+      //         },
+      //     ],
+      //     include: paths.appSrc,
+      // },
+      {
+        exclude: [
+          /\.html$/,
+          /\.(js|jsx)$/,
+          /\.css$/,
+          /\.less$/,
+          /\.json$/,
+          /\.bmp$/,
+          /\.gif$/,
+          /\.jpe?g$/,
+          /\.png$/
         ],
-    },
-    module: {
-        strictExportPresence: true,
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        options: {
-                            formatter: eslintFormatter,
-                            baseConfig: {
-                                extends: [require.resolve(path.resolve(__dirname, "../config/eslint-config-react"))],
-                            },
-                            ignore: false,
-                            useEslintrc: false
-                        },
-                        loader: require.resolve('eslint-loader'),
-                    },
-                ],
-                include: paths.appSrc,
-            },
-            {
-                exclude: [
-                    /\.html$/,
-                    /\.(js|jsx)$/,
-                    /\.css$/,
-                    /\.less$/,
-                    /\.json$/,
-                    /\.bmp$/,
-                    /\.gif$/,
-                    /\.jpe?g$/,
-                    /\.png$/,
-                ],
-                loader: require.resolve('file-loader'),
-                options: {
-                    name: 'static/media/[name].[hash:8].[ext]',
-                },
-            },
-            {
-                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                loader: require.resolve('url-loader'),
-                options: {
-                    limit: 10000,
-                    name: 'static/media/[name].[hash:8].[ext]',
-                },
-            },
-            {
-                test: /\.(js|jsx)$/,
-                include: paths.appSrc,
-                exclude: /node_modules/,
-                loader: 'babel-loader?cacheDirectory=true'
-            },
-            {
-                test: /\.(less|css)$/,
-                use: [
-                    require.resolve('style-loader'),
-                    {
-                        loader: require.resolve('css-loader'),
-                        options: {
-                            importLoaders: 1,
-                        },
-                    },
-                    {
-                        loader: require.resolve('postcss-loader'),
-                        options: {
-                            ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
-                            plugins: () => [
-                                require('postcss-flexbugs-fixes'),
-                                autoprefixer({
-                                    browsers: [
-                                        '>1%',
-                                        'last 4 versions',
-                                        'Firefox ESR',
-                                        'not ie < 9', // React doesn't support IE8 anyway
-                                    ],
-                                    flexbox: 'no-2009',
-                                }),
-                            ],
-                        },
-                    },
-                    {
-                        loader: require.resolve('less-loader'),
-                        options: {
-                            modifyVars: { "@primary-color": "#1DA57A" },
-                        },
-                    }
-                ],
-            },
-        ],
-    },
-    plugins: [
-        new InterpolateHtmlPlugin(env.raw),
-        new webpack.ProvidePlugin({
-            _: "lodash",
-            "window._": "lodash",
-            "Log": 'hefan-log'
-        }),
-    ],
-    node: {
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty',
-    }
+        loader: require.resolve("file-loader"),
+        options: {
+          name: "static/media/[name].[hash:8].[ext]"
+        }
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve("url-loader"),
+        options: {
+          limit: 10000,
+          name: "static/media/[name].[hash:8].[ext]"
+        }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        include: paths.appSrc,
+        exclude: /node_modules/,
+        loader: "babel-loader?cacheDirectory=true"
+      },
+      {
+        test: /\.(less|css)$/,
+        use: [
+          require.resolve("style-loader"),
+          {
+            loader: require.resolve("css-loader"),
+            options: {
+              importLoaders: 1
+            }
+          },
+          {
+            loader: require.resolve("postcss-loader"),
+            options: {
+              ident: "postcss", // https://webpack.js.org/guides/migrating/#complex-options
+              plugins: () => [
+                require("postcss-flexbugs-fixes"),
+                autoprefixer({
+                  browsers: [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "not ie < 9" // React doesn't support IE8 anyway
+                  ],
+                  flexbox: "no-2009"
+                })
+              ]
+            }
+          },
+          {
+            loader: require.resolve("less-loader"),
+            options: {
+              modifyVars: { "@primary-color": "#1DA57A" }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new InterpolateHtmlPlugin(env.raw),
+    new webpack.ProvidePlugin({
+      _: "lodash",
+      "window._": "lodash",
+      Log: "hefan-log"
+    })
+  ],
+  node: {
+    fs: "empty",
+    net: "empty",
+    tls: "empty"
+  }
 };
